912d9955a5a3e6b2ee90154c2674b86d21744c08
Abdullah Alfallaj <46138890+afalfallaj@users.noreply.github.com>
2022-10-18 23:52:14 +0300

    get device status from query

--- custom_components/gimdow/gimdow.py
+++ custom_components/gimdow/gimdow.py
@@ -1,13 +1,14 @@
 from tuya_connector import (
 	TuyaOpenAPI
 )
+import time  
 
 class GimdowInstance:
     def __init__(self, lock: dict) -> None:
         self._device_id = lock["device_id"]
         self._openapi = TuyaOpenAPI(lock["tuya_endpoint"], lock["access_id"], lock["access_key"])
         self.is_locked:bool | None = None
-        # self._connected = None
+        self._latest_update:int = self.get_timestamp()
     
     def set_lock(self,state: bool):
         self._openapi.connect()
@@ -25,13 +26,29 @@ class GimdowInstance:
                     self.is_locked = state
                     return self.is_locked
     def update(self):
+        start_time= self._latest_update
+        end_time = self.get_timestamp()
+
         self._openapi.connect()
-        status_result= self._openapi.get(
-            f"/v1.0/iot-03/devices/{self._device_id}/status")
+        query_result= self._openapi.get(
+            f"/v1.0/devices/{self._device_id}/logs?end_time={end_time}&start_time={start_time}&type=7")
 
-        status_list = status_result.get("result")
+        query_list = query_result.get("result")
 
-        if status_list:
-            lock_motor_state = [status for status in status_list if status.get("code") == 'lock_motor_state']
-            if len(lock_motor_state):
-                self.is_locked = not lock_motor_state[0].get("value")
+        if query_list.get("logs"):
+            self._latest_update = end_time
+            for logs in query_list.get("logs"):
+                if logs.get("code") == 'lock_motor_state':
+                    self.is_locked = not logs.get("value")
+                    break
+                elif logs.get("code") == 'lock_record':
+                    self.is_locked = True
+                    break
+                elif logs.get("code") == 'unlock_key':
+                    self.is_locked = False
+                    break
+        else:
+            #if no logs found then get results from 30 days ago
+            self._latest_update = start_time - 2592000000
+    def get_timestamp(self):
+        return int(time.time() *1000)
